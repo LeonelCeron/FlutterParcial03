24088f227d7812204f1034ca5af26feda1a1ad21
LeonelCeron <ceronleonel99@gmail.com>
2022-10-19 21:35:44 -0600

    Primer commit

---
+++ flutter_parcial_03/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_parcial_03/paginas/home.dart';
+
+void main() {
+  runApp(Parcial03());
+}
+
+class Parcial03 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
